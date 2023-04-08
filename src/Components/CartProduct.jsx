3835533835e68eff7e96e5b421b5ae1aca72cfdb
vivek6201 <gvicky4567888@gmail.com>
2023-04-08 23:14:42 +0530

    cart UI Improved

--- src/Components/CartProduct.jsx
+++ src/Components/CartProduct.jsx
@@ -14,16 +14,16 @@ const CartProduct = ({ product }) => {
   }
 
   return (
-    <div className="flex w-full py-3 px-5 rounded-xl border h-[350px]">
-      <div className="flex items-center h-full justify-center w-4/12">
-        <img src={product.image} className="w-8/12 object-contain aspect-square"/>
+    <div className="flex md:flex-row flex-col w-full py-3 px-5 rounded-xl border xs:h-[400px] sm:h-[500px] md:h-[350px]">
+      <div className="flex items-center h-full justify-center w-full md:w-4/12">
+        <img src={product.image} className="w-5/12 md:w-8/12 object-contain aspect-square"/>
       </div>
-      <div className="p-5 flex flex-col justify-between w-8/12 gap-y-3">
-        <p className="text-xl font-bold">{product.title}</p>
-        <p className="opacity-70">{product.description.substring(0,200)}</p>
+      <div className="md:p-5 p-2 flex flex-col justify-between w-full md:w-8/12 gap-y-3">
+        <p className="text-base lg:text-xl font-bold">{product.title}</p>
+        <p className="opacity-70 text-sm">{product.description.substring(0,100)}</p>
         <p className="font-bold text-green-400">${product.price}</p>
 
-        <div className="w-full flex justify-between items-center">
+        <div className="w-full flex lg:flex-row flex-col gap-4 my-4 justify-between items-center">
           <label className="flex gap-x-4">
             <p>Item Count</p>
             <select name="itemCount" className="w-14 text-center" onChange={(e) => setItemCount(e.target.value)} value={itemCount}>
@@ -35,7 +35,7 @@ const CartProduct = ({ product }) => {
             </select>
           </label>
 
-          <button className="rounded-full bg-green-600 text-white py-2 px-4" onClick={removeFromCart}>Remove From Cart</button>
+          <button className="rounded-full bg-green-600 sm:text-sm text-white py-2 px-4" onClick={removeFromCart}>Remove From Cart</button>
         </div>
       </div>
     </div>
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -29,8 +29,8 @@ export const Cart = () => {
           </NavLink>
         </div>
       ) : (
-        <div className="max-w-[1080px] mx-auto flex gap-x-5 h-full">
-          <div className="w-7/12 flex flex-col gap-y-5">
+        <div className="max-w-[1080px] w-11/12 mx-auto flex md:flex-row flex-col gap-5 h-full items-center md:items-start">
+          <div className="w-11/12 md:w-7/12 flex flex-col gap-y-5">
             {
               cart.map((product) =>{
                 return (
@@ -40,7 +40,7 @@ export const Cart = () => {
             }
           </div>
 
-          <div className="w-5/12 py-5 px-10 h-full flex flex-col gap-y-4">
+          <div className="w-11/12 md:w-5/12 py-5 px-10 h-full flex flex-col gap-y-4">
             <h1 className="font-bold text-4xl">Summary</h1>
             <p>Total Item : {cart.length}</p>
             <div className="mt-20 flex flex-col gap-y-3 ">
